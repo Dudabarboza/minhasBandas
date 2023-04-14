7a62578a6fb0480845689483e628909d732e5c57
Maria Eduarda Barboza <108808310+Dudabarboza@users.noreply.github.com>
2023-04-14 14:41:22 -0300

    Add files via upload

---
+++ bandas- equipe5/shadowOfIntent/shadow.js
@@ -0,0 +1,10 @@
+let songsScrollTo = document.getElementById('songs-title')
+let videosScrollTo = document.getElementById('videos-title')
+
+function songsScroll() {
+    songsScrollTo.scrollIntoView(true);
+  }
+
+  function videoScroll() {
+    videosScrollTo.scrollIntoView(true);
+  }
